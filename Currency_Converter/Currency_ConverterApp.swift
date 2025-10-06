1d4f807dded3db6de48bae646a351871e15ffb85
Paul <udegbuepaul43@gmail.com>
2025-10-06 12:33:15 +0100

    Initial Commit

---
+++ Currency_Converter/Currency_ConverterApp.swift
@@ -0,0 +1,20 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency_Converter
+//
+//  Created by Chaos on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
